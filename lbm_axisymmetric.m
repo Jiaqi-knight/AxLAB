a65d32476872f44bb762bb3d69dd1eb667040ceb
José Pedro <1jpsneto@gmail.com>
2016-03-02 13:12:57 -0300

    streaming correct

--- lbm_axisymmetric.m
+++ lbm_axisymmetric.m
@@ -18,7 +18,7 @@ close all
 
 Nr = 300;                    % Number of lines   (cells in the y direction)
 Mc = 300;                    % Number of columns (cells in the x direction)
-
+N_c = 9;
 
 % Block 2
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -47,22 +47,6 @@ visc = cs2*(1/omega-0.5);                         % lattice viscosity
 visc_phy = visc*(Dx^2)/Dt;                        % physical kinematic viscosity
 
 
-% Block 4
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Lattice properties for the D2Q9 model
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-N_c = 9;                                             % number of directions of the D2Q9 model
-% alpha = 0,1,2,3,4,5,6,7,8
-C_x = [0 1 1 0 -1 -1 -1  0 1];                       % velocity vectors in x
-C_y = [0 0 1 1 1 0 -1 -1 -1];                       % velocity vectors in y
-w0=16/36. ; w1=4/36. ; w2=1/36.;                    % lattice weights
-W = [w1 w1 w1 w1 w2 w2 w2 w2 w0];
-f1=3.;
-f2=4.5;
-f3=1.5;                                             % coef. of the f equil.
-
-
 % Array of distribution and relaxation functions
 f=zeros(Nr,Mc,N_c);                                 
 feq=zeros(Nr,Mc,N_c);
@@ -72,18 +56,12 @@ f(:,:,:)=rho_l/9;
 ux = zeros(Nr, Mc);
 uy = zeros(Nr, Mc);
 
-rho_l = 0.01;   % initial disturbance
-f(Nr/2, Mc/2,9) = rho_l;
-
-
 %Block 5
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Begin the iteractive process
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
-
-for ta = 1 : 150*sqrt(3)
+for ta = 1 : round(150*sqrt(3))
     
     % Block 5.1
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -91,14 +69,14 @@ for ta = 1 : 150*sqrt(3)
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
    
     f(:,:,1) = [f(:,1:2,1) f(:,2:Mc-1,1)];
+    f(:,:,2) = [f(:,1:2,2) f(:,2:Mc-1,2)];
     f(:,:,2) = [f(1:2,:,2);f(2:Nr-1,:,2)];
-    f(:,:,3) = [f(:,2:Mc-1,3) f(:,Mc-1:Mc,3)];
-    f(:,:,4) = [f(2:Nr-1,:,4);f(Nr-1:Nr,:,4)];
-    f(:,:,5) = [f(:,1:2,5) f(:,2:Mc-1,5)];
-    f(:,:,5) = [f(1:2,:,5);f(2:Nr-1,:,5)];
+    f(:,:,3) = [f(1:2,:,3);f(2:Nr-1,:,3)];
+    f(:,:,4) = [f(:,2:Mc-1,4) f(:,Mc-1:Mc,4)];
+    f(:,:,4) = [f(1:2,:,4);f(2:Nr-1,:,4)];
+    f(:,:,5) = [f(:,2:Mc-1,5) f(:,Mc-1:Mc,5)];
     f(:,:,6) = [f(:,2:Mc-1,6) f(:,Mc-1:Mc,6)];
-    f(:,:,6) = [f(1:2,:,6);f(2:Nr-1,:,6)];
-    f(:,:,7) = [f(:,2:Mc-1,7) f(:,Mc-1:Mc,7)];
+    f(:,:,6) = [f(2:Nr-1,:,6);f(Nr-1:Nr,:,6)];
     f(:,:,7) = [f(2:Nr-1,:,7);f(Nr-1:Nr,:,7)];
     f(:,:,8) = [f(:,1:2,8) f(:,2:Mc-1,8)];
     f(:,:,8) = [f(2:Nr-1,:,8);f(Nr-1:Nr,:,8)];
@@ -109,36 +87,39 @@ for ta = 1 : 150*sqrt(3)
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % recalculating rho and u
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    if ta == 1
+        f(150 - 20: 150 + 20, 150 - 20: 150 + 20, :) = rho_p;
+    end
     rho=sum(f,3); 
     
 
         
-    rt0= w0*rho;
-    rt1= w1*rho;
-    rt2= w2*rho;
+    %rt0= w0*rho;
+    %rt1= w1*rho;
+    %rt2= w2*rho;
 
     % Determining the velocities according to Eq.() (see slides)    
-    ux = (C_x(1).*f(:,:,1)+C_x(2).*f(:,:,2)+C_x(3).*f(:,:,3)+C_x(4).*f(:,:,4)+C_x(5).*f(:,:,5)+C_x(6).*f(:,:,6)+C_x(7).*f(:,:,7)+C_x(8).*f(:,:,8))./rho ;
-    uy = (C_y(1).*f(:,:,1)+C_y(2).*f(:,:,2)+C_y(3).*f(:,:,3)+C_y(4).*f(:,:,4)+C_y(5).*f(:,:,5)+C_y(6).*f(:,:,6)+C_y(7).*f(:,:,7)+C_y(8).*f(:,:,8))./rho ;
+    %ux = (C_x(1).*f(:,:,1)+C_x(2).*f(:,:,2)+C_x(3).*f(:,:,3)+C_x(4).*f(:,:,4)+C_x(5).*f(:,:,5)+C_x(6).*f(:,:,6)+C_x(7).*f(:,:,7)+C_x(8).*f(:,:,8))./rho ;
+    %uy = (C_y(1).*f(:,:,1)+C_y(2).*f(:,:,2)+C_y(3).*f(:,:,3)+C_y(4).*f(:,:,4)+C_y(5).*f(:,:,5)+C_y(6).*f(:,:,6)+C_y(7).*f(:,:,7)+C_y(8).*f(:,:,8))./rho ;
         
 
     % Block 5.3
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %% Determining the relaxation functions for each direction
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%       
-    uxsq=ux.^2; 
-    uysq=uy.^2; 
-    usq=uxsq+uysq; 
+    %uxsq=ux.^2; 
+    %uysq=uy.^2; 
+    %usq=uxsq+uysq; 
        
-    feq(:,:,1)= rt1 .*(1 +f1*ux +f2.*uxsq -f3*usq);
-    feq(:,:,2)= rt1 .*(1 +f1*uy +f2*uysq -f3*usq);
-    feq(:,:,3)= rt1 .*(1 -f1*ux +f2*uxsq -f3*usq);
-    feq(:,:,4)= rt1 .*(1 -f1*uy +f2*uysq -f3*usq);
-    feq(:,:,5)= rt2 .*(1 +f1*(+ux+uy) +f2*(+ux+uy).^2 -f3.*usq);
-    feq(:,:,6)= rt2 .*(1 +f1*(-ux+uy) +f2*(-ux+uy).^2 -f3.*usq);
-    feq(:,:,7)= rt2 .*(1 +f1*(-ux-uy) +f2*(-ux-uy).^2 -f3.*usq);
-    feq(:,:,8)= rt2 .*(1 +f1*(+ux-uy) +f2*(+ux-uy).^2 -f3.*usq);
-    feq(:,:,9)= rt0 .*(1 - f3*usq);
+    %feq(:,:,1)= rt1 .*(1 +f1*ux +f2.*uxsq -f3*usq);
+    %feq(:,:,2)= rt1 .*(1 +f1*uy +f2*uysq -f3*usq);
+    %feq(:,:,3)= rt1 .*(1 -f1*ux +f2*uxsq -f3*usq);
+    %feq(:,:,4)= rt1 .*(1 -f1*uy +f2*uysq -f3*usq);
+    %feq(:,:,5)= rt2 .*(1 +f1*(+ux+uy) +f2*(+ux+uy).^2 -f3.*usq);
+    %feq(:,:,6)= rt2 .*(1 +f1*(-ux+uy) +f2*(-ux+uy).^2 -f3.*usq);
+    %feq(:,:,7)= rt2 .*(1 +f1*(-ux-uy) +f2*(-ux-uy).^2 -f3.*usq);
+    %feq(:,:,8)= rt2 .*(1 +f1*(+ux-uy) +f2*(+ux-uy).^2 -f3.*usq);
+    %feq(:,:,9)= rt0 .*(1 - f3*usq);
     
     
    
@@ -147,13 +128,15 @@ for ta = 1 : 150*sqrt(3)
     % Collision (relaxation) step
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
    
-    f= (1-omega)*f + omega*feq; 
+    %f= (1-omega)*f + omega*feq; 
     
     
     
 % Ploting the results in real time   
 surf(rho-1), view(2), shading flat, axis equal, caxis([-.00001 .00001])
+%imagesc(rho)
 grid off
-pause(.0001) 
+pause(.0001)
+ta
 
 end %  End main time Evolution Loop
